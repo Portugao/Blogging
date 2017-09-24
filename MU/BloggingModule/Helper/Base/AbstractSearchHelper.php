f2d7cac860603e16a67ef4d61f8856b5c905e6aa
Portugao <info@homepages-mit-zikula.de>
2017-09-24 15:51:16 +0200

    helper update

--- MU/BloggingModule/Helper/Base/AbstractSearchHelper.php
+++ MU/BloggingModule/Helper/Base/AbstractSearchHelper.php
@@ -163,23 +163,21 @@ abstract class AbstractSearchHelper implements SearchableInterface
     
         // retrieve list of activated object types
         $searchTypes = $this->getSearchTypes();
+        $entitiesWithDisplayAction = ['post'];
     
         foreach ($searchTypes as $searchTypeCode => $typeInfo) {
-            $objectType = $typeInfo['value'];
             $isActivated = false;
-            if ($this->request->isMethod('GET')) {
-                $isActivated = $this->request->query->get('active_' . $searchTypeCode, false);
-            } elseif ($this->request->isMethod('POST')) {
-                $searchSettings = $this->request->request->get('zikulasearchmodule_search', []);
-                $moduleActivationInfo = $searchSettings['modules'];
-                if (isset($moduleActivationInfo['MUBloggingModule'])) {
-                    $moduleActivationInfo = $moduleActivationInfo['MUBloggingModule'];
-                    $isActivated = isset($moduleActivationInfo['active_' . $searchTypeCode]);
-                }
+            $searchSettings = $this->request->query->get('zikulasearchmodule_search', []);
+            $moduleActivationInfo = $searchSettings['modules'];
+            if (isset($moduleActivationInfo['MUBloggingModule'])) {
+                $moduleActivationInfo = $moduleActivationInfo['MUBloggingModule'];
+                $isActivated = isset($moduleActivationInfo['active_' . $searchTypeCode]);
             }
             if (!$isActivated) {
                 continue;
             }
+    
+            $objectType = $typeInfo['value'];
             $whereArray = [];
             $languageField = null;
             switch ($objectType) {
@@ -235,13 +233,9 @@ abstract class AbstractSearchHelper implements SearchableInterface
             }
     
             $descriptionFieldName = $this->entityDisplayHelper->getDescriptionFieldName($objectType);
-    
-            $entitiesWithDisplayAction = ['post'];
+            $hasDisplayAction = in_array($objectType, $entitiesWithDisplayAction);
     
             foreach ($entities as $entity) {
-                $urlArgs = $entity->createUrlArgs();
-                $hasDisplayAction = in_array($objectType, $entitiesWithDisplayAction);
-    
                 // perform permission check
                 if (!$this->permissionApi->hasPermission('MUBloggingModule:' . ucfirst($objectType) . ':', $entity->getKey() . '::', ACCESS_OVERVIEW)) {
                     continue;
@@ -258,10 +252,13 @@ abstract class AbstractSearchHelper implements SearchableInterface
                 $description = !empty($descriptionFieldName) ? $entity[$descriptionFieldName] : '';
                 $created = isset($entity['createdDate']) ? $entity['createdDate'] : null;
     
-                $urlArgs['_locale'] = (null !== $languageField && !empty($entity[$languageField])) ? $entity[$languageField] : $this->request->getLocale();
-    
                 $formattedTitle = $this->entityDisplayHelper->getFormattedTitle($entity);
-                $displayUrl = $hasDisplayAction ? new RouteUrl('mubloggingmodule_' . strtolower($objectType) . '_display', $urlArgs) : '';
+                $displayUrl = '';
+                if ($hasDisplayAction) {
+                    $urlArgs = $entity->createUrlArgs();
+                    $urlArgs['_locale'] = (null !== $languageField && !empty($entity[$languageField])) ? $entity[$languageField] : $this->request->getLocale();
+                    $displayUrl = new RouteUrl('mubloggingmodule_' . strtolower($objectType) . '_display', $urlArgs);
+                }
     
                 $result = new SearchResultEntity();
                 $result->setTitle($formattedTitle)
