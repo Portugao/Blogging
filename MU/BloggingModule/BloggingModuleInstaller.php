b896ea57eb4732b266b4b74feebb52fe206c16f7
Portugao <info@homepages-mit-zikula.de>
2017-11-06 15:10:33 +0100

    version 1.0.1

--- MU/BloggingModule/BloggingModuleInstaller.php
+++ MU/BloggingModule/BloggingModuleInstaller.php
@@ -19,5 +19,75 @@ use MU\BloggingModule\Base\AbstractBloggingModuleInstaller;
  */
 class BloggingModuleInstaller extends AbstractBloggingModuleInstaller
 {
-    // feel free to extend the installer here
+    /**
+     * Upgrade the MUBloggingModule application from an older version.
+     *
+     * If the upgrade fails at some point, it returns the last upgraded version.
+     *
+     * @param integer $oldVersion Version to upgrade from
+     *
+     * @return boolean True on success, false otherwise
+     *
+     * @throws RuntimeException Thrown if database tables can not be updated
+     */
+    public function upgrade($oldVersion)
+    {
+        $logger = $this->container->get('logger');
+    
+        // Upgrade dependent on old version number
+        switch ($oldVersion) {
+            case '1.0.0':
+                // do something
+                // ...
+                // update the database schema
+                try {
+                    $this->schemaTool->update($this->listEntityClasses());
+                } catch (\Exception $exception) {
+                    $this->addFlash('error', $this->__('Doctrine Exception') . ': ' . $exception->getMessage());
+                    $logger->error('{app}: Could not update the database tables during the upgrade. Error details: {errorMessage}.', ['app' => 'MUBloggingModule', 'errorMessage' => $exception->getMessage()]);
+    
+                    return false;
+                }
+            case '1.0.1':
+            	// for later use
+        }
+    
+        // Note there are several helpers available for making migrating your extension from Zikula 1.3 to 1.4 easier.
+        // The following convenience methods are each responsible for a single aspect of upgrading to Zikula 1.4.x.
+    
+        // here is a possible usage example
+        // of course 1.2.3 should match the number you used for the last stable 1.3.x module version.
+        /* if ($oldVersion = '1.2.3') {
+            // rename module for all modvars
+            $this->updateModVarsTo14();
+            
+            // update extension information about this app
+            $this->updateExtensionInfoFor14();
+            
+            // rename existing permission rules
+            $this->renamePermissionsFor14();
+            
+            // rename existing category registries
+            $this->renameCategoryRegistriesFor14();
+            
+            // rename all tables
+            $this->renameTablesFor14();
+            
+            // remove event handler definitions from database
+            $this->dropEventHandlersFromDatabase();
+            
+            // update module name in the hook tables
+            $this->updateHookNamesFor14();
+            
+            // update module name in the workflows table
+            $this->updateWorkflowsFor14();
+        } * /
+    
+        // remove obsolete persisted hooks from the database
+        //$this->hookApi->uninstallSubscriberHooks($this->bundle->getMetaData());
+    */
+    
+        // update successful
+        return true;
+    }
 }
