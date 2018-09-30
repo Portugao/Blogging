140d6a9106c5fcf0db093609afe4bda4296b00fe
Michael Ueberschaer <info@homepages-mit-zikula.de>
2018-09-30 18:06:50 +0200

    overriding of field

--- MU/BloggingModule/Form/Type/PostType.php
+++ MU/BloggingModule/Form/Type/PostType.php
@@ -16,6 +16,7 @@ use MU\BloggingModule\Form\Type\Base\AbstractPostType;
 
 use Symfony\Component\Form\FormBuilderInterface;
 use MU\BloggingModule\Form\Type\Field\MultiListType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 /**
  * Post editing form type implementation class.
@@ -31,6 +32,28 @@ class PostType extends AbstractPostType
     public function addEntityFields(FormBuilderInterface $builder, array $options = [])
     {
     	parent::addEntityFields($builder, $options);
+    	
+    	$listEntries = $this->listHelper->getEntries('post', 'positionOfBlock');
+    	$choices = [];
+    	$choiceAttributes = [];
+    	foreach ($listEntries as $entry) {
+    	    $choices[$entry['text']] = $entry['value'];
+    	    $choiceAttributes[$entry['text']] = ['title' => $entry['title']];
+    	}
+    	$builder->add('positionOfBlock', ChoiceType::class, [
+    	    'label' => $this->__('Position of block') . ':',
+    	    'empty_data' => 'none',
+    	    'attr' => [
+    	        'class' => 'bloggerblock',
+    	        'title' => $this->__('Choose the position of block.')
+    	    ],
+    	    'required' => false,
+    	    'placeholder' => $this->__('Choose an option'),
+    	    'choices' => $choices,
+    	    'choice_attr' => $choiceAttributes,
+    	    'multiple' => false,
+    	    'expanded' => false
+    	]);
         
         $listEntries = $this->listHelper->getEntries('post', 'similarArticles');
         $choices = [];
