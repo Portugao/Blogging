4e48527d64fb292de810be589cf54b526dc958e5
Michael Ueberschaer <info@homepages-mit-zikula.de>
2018-10-01 10:33:19 +0200

    fixed #10

--- MU/BloggingModule/Block/Form/Type/ItemListBlockType.php
+++ MU/BloggingModule/Block/Form/Type/ItemListBlockType.php
@@ -14,10 +14,35 @@ namespace MU\BloggingModule\Block\Form\Type;
 
 use MU\BloggingModule\Block\Form\Type\Base\AbstractItemListBlockType;
 
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+
 /**
  * List block form type implementation class.
  */
 class ItemListBlockType extends AbstractItemListBlockType
 {
+    /**
+     * Adds a sorting field.
+     *
+     * @param FormBuilderInterface $builder The form builder
+     * @param array                $options The options
+     */
+    public function addSortingField(FormBuilderInterface $builder, array $options = [])
+    {
+        $builder->add('sorting', ChoiceType::class, [
+            'label' => $this->__('Sorting', 'mubloggingmodule') . ':',
+            'empty_data' => 'default',
+            'choices' => [
+                $this->__('Random', 'mubloggingmodule') => 'random',
+                $this->__('Newest', 'mubloggingmodule') => 'newest',
+                $this->__('Updated', 'mubloggingmodule') => 'updated',
+                $this->__('Start date', 'mubloggingmodule') => 'startdate',
+                $this->__('Default', 'mubloggingmodule') => 'default'
+            ],
+            'multiple' => false,
+            'expanded' => false
+        ]);
+    }
     // feel free to extend the list block form type class here
 }
